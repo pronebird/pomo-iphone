895f0c8e910eeaab571717219a815a4884c5fc85
Andrej Mihajlov <and@codeispoetry.ru>
2014-08-24 16:44:16 +0200

    Add overloaded gettext helper functions

--- pomo-iphone/GettextHelpers.h
+++ pomo-iphone/GettextHelpers.h
@@ -6,19 +6,118 @@
 //  Copyright (c) 2012 Andrej Mihajlov. All rights reserved.
 //
 
+#import "TranslationCenter.h"
+
 #define DEFAULT_TEXTDOMAIN @"default"
+#define OVERLOADABLE_FUNC __attribute__((overloadable))
 
-// Shorthandles that use DEFAULT_TEXTDOMAIN
-NSString* __(NSString* singular);
-NSString* _x(NSString* singular, NSString* context);
-NSString* _nx(NSString* singular, NSString* plural, NSInteger n, NSString* context);
-NSString* _n(NSString* singular, NSString* plural, NSInteger n);
+#if !__has_extension(attribute_overloadable)
+#error GettextHelpers require overloadable extension
+#endif
 
-NSString* ___(NSString* singular, NSString* domain);
-NSString* __x(NSString* singular, NSString* context, NSString* domain);
-NSString* __nx(NSString* singular, NSString* plural, NSInteger n, NSString* context, NSString* domain);
-NSString* __n(NSString* singular, NSString* plural, NSInteger n, NSString* domain);
+/**
+ *  Translate a string using provided text domain
+ *
+ *  @param singular an original string
+ *  @param domain   a text domain
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _(NSString* singular, NSString* domain) OVERLOADABLE_FUNC {
+	return [[TranslationCenter sharedCenter] translate:singular domain:domain];
+};
 
-// @TODO: find a way for noops
-//#define _n_noop(a, b) ()
-//#define _nx_noop(a, b) ()
+/**
+ *  Translate a string from default text domain
+ *
+ *  @param singular an original string
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _(NSString* singular) OVERLOADABLE_FUNC {
+	return _(singular, DEFAULT_TEXTDOMAIN);
+};
+
+/**
+ *  Translate a string using provided text domain and context
+ *
+ *  @param singular an original string
+ *  @param context  a context
+ *  @param domain   a text domain
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _x(NSString* singular, NSString* context, NSString* domain) OVERLOADABLE_FUNC {
+	return [[TranslationCenter sharedCenter] translate:singular context:context domain:domain];
+};
+
+/**
+ *  Translate a string using context and default text domain
+ *
+ *  @param singular an original string
+ *  @param context  a context
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _x(NSString* singular, NSString* context) OVERLOADABLE_FUNC {
+	return _x(singular, context, DEFAULT_TEXTDOMAIN);
+};
+
+/**
+ *  Translate a string respecting provided number, using context and text domain
+ *
+ *  @param singular a singular string
+ *  @param plural   a plural string
+ *  @param n        a number used to lookup a translation with correct inflection
+ *  @param context  a context
+ *  @param domain   a text domain
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _nx(NSString* singular, NSString* plural, NSInteger n, NSString* context, NSString* domain) OVERLOADABLE_FUNC {
+	return [[TranslationCenter sharedCenter] translatePlural:singular plural:plural count:n context:context domain:domain];
+};
+
+/**
+ *  Translate a string respecting provided number, using context and default text domain
+ *  @param singular a singular string
+ *  @param plural   a plural string
+ *  @param n        a number used to lookup a translation with correct inflection
+ *  @param context  a context
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _nx(NSString* singular, NSString* plural, NSInteger n, NSString* context) OVERLOADABLE_FUNC {
+	return _nx(singular, plural, n, context, DEFAULT_TEXTDOMAIN);
+};
+
+/**
+ *  Translate a string respecting provided number, using default text domain
+ *
+ *  @param singular a singular string
+ *  @param plural   a plural string
+ *  @param n        a number used to lookup a translation with correct inflection
+ *  @param domain   a text domain
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _n(NSString* singular, NSString* plural, NSInteger n, NSString* domain) OVERLOADABLE_FUNC {
+	return [[TranslationCenter sharedCenter] translatePlural:singular plural:plural count:n context:nil domain:DEFAULT_TEXTDOMAIN];
+};
+
+/**
+ *  Translate a string respecting provided number, using default text domain
+ *
+ *  @param singular a singular string
+ *  @param plural   a plural string
+ *  @param n        a number used to lookup a translation with correct inflection
+ *
+ *  @return a translated string on success, otherwise original string
+ */
+static inline NSString* _n(NSString* singular, NSString* plural, NSInteger n) OVERLOADABLE_FUNC {
+	return _n(singular, plural, n, DEFAULT_TEXTDOMAIN);
+};
+
+
+#define _n_noop(singular, plural, domain)
+#define _nx_noop(singular, plural, context, domain)
