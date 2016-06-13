d571820b0fb17f6ae455d31546dac40e521a3d04
Andrej Mihajlov <and@codeispoetry.ru>
2016-06-13 12:00:01 +0100

    Refactor code and improve nullability

--- pomo-iphone/TranslationCenter.h
+++ pomo-iphone/TranslationCenter.h
@@ -11,27 +11,27 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TranslationCenter : NSObject {
-    NSMutableDictionary* _domains;
-	NSString* _defaultPath;
-	NSString* _language;
+    NSMutableDictionary *_domains;
+	NSString *_defaultPath;
+	NSString *_language;
 }
 
-@property (readwrite, nonatomic, copy) NSString* defaultPath;
-@property (readwrite, nonatomic, copy) NSString* language;
-@property (readonly, nonatomic, strong) NSMutableDictionary* domains;
+@property (readwrite, nonatomic, copy) NSString *defaultPath;
+@property (readwrite, nonatomic, copy) NSString *language;
+@property (readonly, nonatomic, strong) NSMutableDictionary *domains;
 
 + (instancetype)sharedCenter;
-+ (NSString*)textDomainFileWithBasePath:(NSString*)path forDomain:(NSString*)domain language:(NSString*)language type:(NSString*)ext;
++ (NSString *)textDomainFileWithBasePath:(NSString *)path forDomain:(NSString *)domain language:(NSString *)language type:(NSString *)ext;
 
-- (BOOL)isValidTextDomain:(NSString*)domain;
+- (BOOL)isValidTextDomain:(NSString *)domain;
 
-- (BOOL)loadTextDomain:(NSString*)domain;
-- (BOOL)loadTextDomain:(NSString*)domain path:(NSString*)path;
-- (BOOL)unloadTextDomain:(NSString*)domain;
+- (BOOL)loadTextDomain:(NSString *)domain;
+- (BOOL)loadTextDomain:(NSString *)domain path:(NSString *)path;
+- (BOOL)unloadTextDomain:(NSString *)domain;
 
-- (NSString*)translate:(NSString*)singular domain:(NSString*)domain;
-- (NSString*)translate:(NSString*)singular context:(NSString*)context domain:(NSString*)domain;
-- (NSString*)translatePlural:(NSString*)singular plural:(NSString*)plural count:(NSInteger)count context:(NSString*)context domain:(NSString*)domain;
+- (NSString *)translate:(NSString *)singular domain:(NSString *)domain;
+- (NSString *)translate:(NSString *)singular context:(nullable NSString *)context domain:(NSString *)domain;
+- (NSString *)translatePlural:(NSString *)singular plural:(NSString *)plural count:(NSInteger)count context:(nullable NSString *)context domain:(NSString *)domain;
 
 @end
 
