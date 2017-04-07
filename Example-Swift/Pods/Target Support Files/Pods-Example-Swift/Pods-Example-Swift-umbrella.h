cebef80eae6b53bfc143678c82bf96714dc7d002
Andrej Mihajlov <and@codeispoetry.ru>
2017-04-07 11:09:46 +0100

    Delete Pods/ from repository

--- Example-Swift/Pods/Target Support Files/Pods-Example-Swift/Pods-Example-Swift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Example_SwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Example_SwiftVersionString[];
-
--- Example-Swift/Pods/Target Support Files/pomo-iphone/pomo-iphone-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_pomo_iphone : NSObject
-@end
-@implementation PodsDummy_pomo_iphone
-@end
